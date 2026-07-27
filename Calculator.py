bb98a512cc56a6c19356e23e95a8e4af6ba547c4
Urju <g.urju2004@gmail.com>
2026-07-27 12:40:50 +0400

    We click on values and display them

--- Calculator.py
+++ Calculator.py
@@ -9,7 +9,7 @@ button_values = [
 ]
 
 right_symbols = ["÷", "×", "-", "+", "="]
-top_symbols = ["C", "+/-", "%", "÷"]
+top_symbols = ["AC", "+/-", "%", "÷"]
 
 row_count = len(button_values) #5
 column_count = len(button_values[0]) #4
@@ -27,8 +27,9 @@ window.resizable(False, False)
 
 frame = tkinter.Frame(window)
 label = tkinter.Label(frame, text="0", font=("Arial", 45), background=color_black,
-                      foreground=color_white)
-label.grid(row=0, column=0)
+                      foreground=color_white, anchor="e")
+
+label.grid(row=0, column=0, columnspan=column_count, sticky="we")
 
 for row in range(row_count):
     for column in range(column_count):
@@ -36,8 +37,53 @@ for row in range(row_count):
       button = tkinter.Button(frame, text=value, font=("Arial", 30),
                               width=column_count-1, height=1,
                               command=lambda value=value: button_clicked(value))
+      #Give buttons color
+      if value in top_symbols:
+         button.config(foreground=color_black, background=color_light_gray)
+      elif value in right_symbols:
+         button.config(foreground=color_white, background=color_orange)
+      else:
+         button.config(foreground=color_white, background=color_dark_gray)   
+
       button.grid(row=row+1, column=column)
 frame.pack()
+
+#A+B, A-B, A*B, A/B
+A = "0"
+operator = None
+B = None
+
+def button_clicked(value):
+   global right_symbols, top_symbols, label, A, B, operator
+
+   if value in right_symbols:
+       pass
+   elif value in top_symbols:
+       pass
+   else: #Digits or .
+       if value == ".":
+           pass
+       elif value in "0123456789":
+           if label["text"] == "0":
+               label["text"] = value #Replace 0
+           else:
+               label["text"] += value #Append digit    
+     
+
+#Center the window
+window.update() #Update the window with the new size dimensions
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+
+#Format "(width)x(height)+(x)+(y)"
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+
 
 window.mainloop() # Runs the GUI application
 
